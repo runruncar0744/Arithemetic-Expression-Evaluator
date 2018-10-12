935707cb25c4f3a4b63433d6513952a3db56cdf2
popshia <B123292179@gmail.com>
2018-10-12 10:11:45 +0800

    Initial Commit

---
+++ Arithemetic Expression Evaluator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Arithemetic Expression Evaluator
+//
+//  Created by popshia on 2018/10/12.
+//  Copyright © 2018 popshia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
